572dfcbcd870dbb870f38d04d1edd966e66d904c
Volodymyr Chernyavskyi <vovikche@gmail.com>
2023-09-05 13:06:05 +0300

    chore (scripts): removed unnesessary scripts

--- Assets/Scripts/Network/Unity/Receivers and Senders/NetworkDataSender.cs
+++
@@ -1,9 +0,0 @@
-using Network.UnityComponents;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class NetworkDataSender : MonoBehaviour
-{
-    public NetworkInitializer NetworkManager = default;
-}
